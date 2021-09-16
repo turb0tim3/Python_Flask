786a39e026901733ef48a2ec3c99eac6d9182de3
Mr.executioner <resimpostasi@gmail.com>
2021-09-16 23:43:45 +0300

    Almost there

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,render_template,flash,request,redirect,url_for
 from flask.helpers import url_for
 from flask_login import login_required,current_user
-from .models import Post,User
+from .models import Post,User,Comment
 from . import db
 views = Blueprint("views",__name__)
 
@@ -49,8 +49,30 @@ def delete_post(id):
 @login_required
 def posts(username):
     user = User.query.filter_by(username=username).first()
+
     if not user:
-        flash("No user with that username exists.",category="error")
-        return redirect(url_for("views.home"))
-    post = Post.query.filter_by(username=username).all()
-    return render_template("posts.html",user=current_user,posts=posts,username=username)
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = user.posts
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
